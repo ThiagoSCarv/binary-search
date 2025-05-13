6ca91a04fe8849d684a877181f1466217ba8a3f3
Thiago Soares <thiagocarv4@gmail.com>
2025-05-12 21:06:08 -0300

    tentando entender two pointers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const array = [1, 3, 4, 5, 7, 10];
+/*const array = [1, 3, 4, 5, 7, 10];
 
 let ponteiroDoInicio = 0;
 
@@ -18,4 +18,36 @@ while (ponteiroDoInicio < ponteiroDoFinal) {
   } else {
     ponteiroDoFinal--;
   }
+}*/
+
+/*const array = [1, 1, 2, 2, 3];
+
+let posicao0 = 0
+
+for (let posicao1 = 1; posicao1 < array.length; posicao1++) {
+  if (array[posicao0] !== array[posicao1]) {
+    posicao0++
+    array[posicao0] = array[posicao1]
+  }
 }
+
+console.log(array.slice(0, posicao0 + 1))*/
+
+/*const array0 = [1, 2, 4, 5, 6]
+
+const array1 = [2, 5, 7]
+
+const numerosEmComum = []
+
+let ponteiroDoArray1 = 0
+
+
+for (let ponteiroDoArray0 = 0; ponteiroDoArray0 < array0.length; ponteiroDoArray0++){
+  if (array0[ponteiroDoArray0] === array1[ponteiroDoArray1]) {
+    numerosEmComum.push(array1[ponteiroDoArray1])
+    ponteiroDoArray1++
+  }
+}
+
+console.log(numerosEmComum)*/
+
